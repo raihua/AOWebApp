c5dd55a18d3c376a495468c05324aa496e0f53d4
raihua <raihua.dev@gmail.com>
2025-08-09 22:03:21 +0930

    Add Item model validation

--- AOWebApp/Models/Item.cs
+++ AOWebApp/Models/Item.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AOWebApp.Models;
 
 public partial class Item
 {
+    [Key]
     public int ItemId { get; set; }
 
+    [Required(ErrorMessage = "Item name is required.")]
+    [StringLength(150, ErrorMessage = "Item name cannot exceed 150 characters.")]
+    [Display(Name = "Item Name")]
     public string ItemName { get; set; } = null!;
 
+    [Required(ErrorMessage = "Item description is required.")]
+    [StringLength(500, ErrorMessage = "Item description cannot exceed 500 characters.")]
+    [Display(Name = "Item Description")]
     public string ItemDescription { get; set; } = null!;
 
+    [Required(ErrorMessage = "Item cost is required.")]
+    [Column(TypeName = "decimal(10, 2)")]
+    [DataType(DataType.Currency)]
+    [Display(Name = "Item Cost")]
     public decimal ItemCost { get; set; }
-
+    
+    [Required(ErrorMessage = "Item image is required.")]
+    [StringLength(255, ErrorMessage = "Item image path cannot exceed 255 characters.")]
+    [Display(Name = "Item Image")]
     public string ItemImage { get; set; } = null!;
 
-    public int CategoryId { get; set; }
+    public int? CategoryId { get; set; }
 
     public virtual ItemCategory Category { get; set; } = null!;
 
