5c12b711826a66fa7a64e495ac911ee663c674ee
raihua <raihua.dev@gmail.com>
2025-08-09 21:32:39 +0930

    Add AmazonOrdersContext and scaffold models

--- AOWebApp/Program.cs
+++ AOWebApp/Program.cs
@@ -6,8 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 builder.Services.AddDbContext<AOWebAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AOWebAppContext") ?? throw new InvalidOperationException("Connection string 'AOWebAppContext' not found.")));
+
+builder.Services.AddDbContext<AOWebAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AmazonOrdersContext") ?? throw new InvalidOperationException("Connection string 'AmazonOrdersContext' not found.")));
 
 var app = builder.Build();
 
