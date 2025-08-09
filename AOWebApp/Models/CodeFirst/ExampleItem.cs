34626ee82fc8c6149b03ff5948cc969cef656e0d
raihua <raihua.dev@gmail.com>
2025-08-09 20:24:13 +0930

    Add CodeFirst ExampleItem model

---
+++ AOWebApp/Models/CodeFirst/ExampleItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AOWebApp.Models.CodeFirst
+{
+    public class ExampleItem
+    {
+        [Key]
+        public int itemId { get; set; }
+
+        [Required]
+        [Display(Name = "Item Name")]
+        [StringLength(100, ErrorMessage = "Item name must be less than 100 characters.")]
+        public string itemName { get; set; }
+
+        [Required]
+        [Range(1, 10000, ErrorMessage = "Item price must be between 1 and 100000.")]
+        [Display(Name = "Item Price")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal itemPrice { get; set; }
+    }
+}
