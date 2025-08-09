ffa802e51e89ef80adb21a90f28113a04d3ea72b
raihua <raihua.dev@gmail.com>
2025-08-09 18:17:20 +0930

    Add Home RazorTest setup task

--- AOWebApp/Controllers/HomeController.cs
+++ AOWebApp/Controllers/HomeController.cs
@@ -31,6 +31,11 @@ namespace AOWebApp.Controllers
             return View();
         }
 
+        public IActionResult RazorTest()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
