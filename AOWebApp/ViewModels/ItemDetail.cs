8b756b5a9df4c53254a6ff986ce0b94a977cda50
raihua <raihua.dev@gmail.com>
2025-08-13 17:52:37 +0930

    Add ItemDetail view model

---
+++ AOWebApp/ViewModels/ItemDetail.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AOWebApp.ViewModels
+{
+    public class ItemDetail
+    {
+        public required Models.Item TheItem { get; set; }
+        public int NumberOfReviews { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        public double AverageRating { get; set; }
+    }
+}
