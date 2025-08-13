faec44becdd33b3f374cf793a093fd105ca4f566
raihua <raihua.dev@gmail.com>
2025-08-13 14:56:19 +0930

    Add ItemSearch view model

---
+++ AOWebApp/ViewModels/ItemSearch.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AOWebApp.ViewModels
+{
+    public class ItemSearch
+    {
+        public string SearchText { get; set; } = string.Empty;
+        public int? CategoryId { get; set; }
+        public SelectList CategoryList { get; set; }
+        public List<Models.Item> Items { get; set; }
+
+    }
+}
