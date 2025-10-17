02ccca1ced88f118e55d46c05ed584336ec44efb
raihua <raihua.dev@gmail.com>
2025-10-18 01:16:38 +1030

    Add Report JSON to Reports Controller

--- AOWebApp/Controllers/ReportsController.cs
+++ AOWebApp/Controllers/ReportsController.cs
@@ -1,6 +1,7 @@
 ﻿using AOWebApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace AOWebApp.Controllers
 {
@@ -22,5 +23,41 @@ namespace AOWebApp.Controllers
 
             return View("AnnualSalesReport", new SelectList(distinctYears));
         }
+
+        [Produces("application/json")]
+        public IActionResult AnnualSalesReportData(int year)
+        {
+            if (year > 0)
+            {
+                var numItemsSold = _context.ItemsInOrders
+                    .Join(_context.CustomerOrders,
+                    iio => iio.OrderNumber,
+                    co => co.OrderNumber,
+                    (iio, co) => new { ItemsInOrder = iio, CustomerOrders = co })
+                    .Where(record => record.CustomerOrders.OrderDate.Year == year)
+                    .GroupBy(record => new
+                    {
+                        year = record.CustomerOrders.OrderDate.Year,
+                        month = record.CustomerOrders.OrderDate.Month,
+                    })
+                    .Select(orderGroup => new
+                    {
+                        year = orderGroup.Key,
+                        monthNo = orderGroup.Key.month,
+                        monthName = "",
+                        totalItems = orderGroup.Sum(orderGroup => orderGroup.ItemsInOrder.NumberOf),
+                        totalSales = orderGroup.Sum(orderGroup => orderGroup.ItemsInOrder.TotalItemCost)
+                    })
+                    .OrderBy(record => record.monthNo)
+                    .ToList(); 
+
+                return Json(numItemsSold);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
     }
+
 }
