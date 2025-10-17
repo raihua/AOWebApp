19134f856e9349fe833098c19d8405db55157c40
raihua <raihua.dev@gmail.com>
2025-10-17 23:22:58 +1030

    Add Reports Controller and AnnualSalesReport view

---
+++ AOWebApp/Controllers/ReportsController.cs
@@ -0,0 +1,19 @@
+﻿using AOWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AOWebApp.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly AmazonOrdersContext _context;
+
+        public ReportsController(AmazonOrdersContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View("AnnualSalesReport");
+        }
+    }
+}
