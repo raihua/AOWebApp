e44135a98938e0932d205a3da071e5d26f54529e
raihua <raihua.dev@gmail.com>
2025-10-21 22:30:04 +1030

    ReactJS Activity

---
+++ AOWebApp/aowebapp/aowebapp/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/Card';
+import CardV2 from './components/CardV2';
+import CardV3 from './components/CardV3';
+import CardList from './components/CardList';
+
+function App() {
+    return (
+        <div className="App container">
+            <div className="bg-light py-1 mb-2">
+                <h2 className="text-center">Example Application</h2>
+            </div>
+            <div className="row justify-content-center">
+                <Card 
+                itemName="test record1"
+                itemImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShEMAMxnxTtczFKdvDlvlmIk86pkZwvIDpkg&s"
+                />
+                <CardV2
+                    itemName="test record2"
+                    itemDescription="test record 2 Desc"
+                    itemImage="https://upload.wikimedia.org/wikipedia/commons/0/04/So_happy_smiling_cat.jpg"
+                    itemCost="10.00"
+                />
+                <CardV3
+                    itemName="test record2"
+                    itemDescription="test record 2 Desc"
+                    itemImage="https://upload.wikimedia.org/wikipedia/commons/0/04/So_happy_smiling_cat.jpg"
+                    itemCost="10.00"
+                />
+                <CardList/>
+            </div>
+        </div>
+    )
+
+}
+
+export default App;
---
+++ AOWebApp/aowebapp/aowebapp/src/components/CardV2.js
@@ -0,0 +1,15 @@
+﻿import React from "react";
+
+function CardV2({itemId, itemName, itemDescription, itemCost, itemImage }) {
+    return (
+        <div className="card" style={{ width: '18rem' }}>
+            <img src={itemImage} className="card-img-top" alt={"Image of " + itemName} />
+            <div className="card-body">
+                <h5 className="card-title">{itemName}</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="/" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>    )
+} 
+
+export default CardV2;
